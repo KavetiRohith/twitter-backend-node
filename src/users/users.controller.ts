b58ee17fd483298a8eed56588a010ca269423439
KavetiRohith <rohitkaveti@gmail.com>
2021-07-17 21:07:02 +0530

    add user repository and service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,19 +2,27 @@ import {
   Controller,
   Delete,
   Get,
+  NotFoundException,
   Param,
   Patch,
   Post,
   Put,
 } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { UsersService } from './users.service';
 
 @ApiTags('users')
 @Controller('users')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
+
   @Get('/@:username')
-  getUserByUsername(@Param('username') username: string): string {
-    return `details of username = ${username}`;
+  async getUserByUsername(@Param('username') username: string): Promise<any> {
+    const user = await this.usersService.getUserByUsername(username);
+    if (!user) {
+      throw new NotFoundException('User Not Found');
+    }
+    return user;
   }
 
   @Get('/:userid')
